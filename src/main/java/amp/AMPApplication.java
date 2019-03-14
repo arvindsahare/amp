6112b9ed6e23b4d05dc75def9d9bf46150d3c562
arvindsahare <arvind.sahare@talentica.com>
2019-03-14 12:53:57 +0530

    Enabled tomcat deployment

--- src/main/java/amp/AMPApplication.java
+++ src/main/java/amp/AMPApplication.java
@@ -2,6 +2,7 @@ package amp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("bloomfilter")
-public class AMPApplication 
+public class AMPApplication extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
